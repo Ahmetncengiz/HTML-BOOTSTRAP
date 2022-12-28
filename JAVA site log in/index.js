761456e9c57958643d2211f6f2fdf7395a5f5c47
Ahmetncengiz <119484222+Ahmetncengiz@users.noreply.github.com>
2022-12-28 21:24:11 +0300

    Add files via upload

---
+++ JAVA site log in/index.js
@@ -0,0 +1,72 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const age = document.getElementById('age');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const ageValue = age.value.trim();
+    const messageValue = message.value.trim();
+
+    if (usernameValue === '') {
+        setError(username, 'Kullanıcı Adı Boş Bırakılamaz');
+    } else {
+        setSuccess(username);
+    }
+
+    if (emailValue === '') {
+        setError(email, 'E Posta Boş Bırakılamaz!');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Geçerli Bir E Posta Giriniz');
+    } else {
+        setSuccess(email);
+    }
+
+    if (ageValue === '') {
+        setError(age, 'Yaş Boş Bırakılamaz');
+    } else if (ageValue.length < 2) {
+        setError(age, 'Yaş En Fazla 2 Sayı Olmalıdır!')
+    } else {
+        setSuccess(age);
+    }
+
+    if (messageValue === '') {
+        setError(message, 'Mesaj Giriniz');
+    } else if (messageValue !== messageValue) {
+        setError(message,);
+    } else {
+        setSuccess(message);
+    }
+
+};
